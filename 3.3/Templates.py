bfe22fb5534aeef6f50fd9eba2d1d08da7de2bb8
MasonN808 <mason.nakamura1@marist.edu>
2021-09-12 18:52:58 -0400

    Bootstrap done

---
+++ 3.3/Templates.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+if __name__ == "__main__":
+    # app.run(host='0.0.0.0', port = 5000, debug=True)
+    app.run(debug = True)
---
+++ 3.4/Bootstrap.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port = 5000, debug=True)
+    # app.run(debug=True)

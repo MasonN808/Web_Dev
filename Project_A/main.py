e6dbace34a308ba0268ed935f1b2c3ddc5e7d803
Mason Nakamura <Mason.Nakamura1@marist.edu>
2021-09-21 20:24:24 -1000

    more edits

--- Project_A/main.py
+++ Project_A/main.py
@@ -7,11 +7,16 @@ from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from models import User
+from auth import auth as auth_blueprint
+from main import main as main_blueprint
+from flask import Blueprint, render_template, flash
+from flask_login import login_required, current_user
+from __init__ import create_app
 import pandas as pd
-
 
 # Initialize the SQLAlchemy
 db = SQLAlchemy()
+
 
 def make_app():
     app = Flask(__name__)
@@ -24,10 +29,29 @@ def make_app():
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
+    from users.models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
-@login_manager.user_loader
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(main_blueprint)
+    return app
 
+# Main blueprint
+main = Blueprint('main', __name__)
 
+@main.route('/')
+def index():
+    return 'index'
+
+@main.route('/profile')
+def profile():
+    return 'profile'
+
+# make app
+
+app = create_app()
 
 
 if __name__ == '__main__':

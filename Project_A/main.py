f4076e51952e93b3168014bec1e96908906dd387
MasonN808 <mason.nakamura1@marist.edu>
2021-09-28 17:54:30 -0400

    Functioning Properly

--- Project_A/main.py
+++ Project_A/main.py
@@ -10,19 +10,20 @@ def index():
     return render_template('index.html')
 
 
-@main.route('/login')
-def login():
-    return render_template('login.html')
+# @main.route('/login')
+# def login():
+#     return render_template('login.html')
 
 
 @main.route('/profile')
+@login_required
 def profile():
     return render_template('profile.html', name = current_user.name)
 
 
-@main.route('/signup')
-def signup():
-    return render_template('signup.html')
+# @main.route('/signup')
+# def signup():
+#     return render_template('signup.html')
 
 
 # make app

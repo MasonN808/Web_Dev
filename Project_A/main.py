efbf9826e3fad2eec0b7173b37dba7e9f5fce37e
Mason Nakamura <Mason.Nakamura1@marist.edu>
2021-10-12 20:43:06 -1000

    New accounts get $50 account amount, working on implementing deposit hyperink.... -Code is operable even with addtions (but unfinished) to deposit progress

--- Project_A/main.py
+++ Project_A/main.py
@@ -16,76 +16,23 @@ main = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
-# Testing of login route
-# @main.route('/login')
-# def login():
-#     return render_template('login.html')
-
 
 @main.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', name = current_user.name, email = current_user.email, password = current_user.password, account_balance = current_user.account_balance)
+    return render_template('profile.html', name = current_user.name, email = current_user.email,
+                           password = current_user.password, account_balance = current_user.account_balance)
 
-# Testing of signup route
-# @main.route('/signup')
-# def signup():
-#     return render_template('signup.html')
-
+@main.route('/deposit')
+@login_required
+def deposit():
+    return render_template('deposit.html')
 
 # make app
 app = create_app()
 
 if __name__ == '__main__':
-    # dbfile = 'data.db'
-    # outfile = open('mydump.csv', 'wb')
-    # outcsv = csv.writer(outfile)
-    # # Create a SQL connection to our SQLite database
-    # con = sqlite3.connect(dbfile)
-    #
-    # # creating cursor
-    # # Now in order to read in pandas dataframe we need to know table name
-    # cursor = con.cursor()
-    # cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    # print(f"Table Name : {cursor.fetchall()}")
-    #
-    # con.close()
-
-    # with open("mydump.csv", 'w') as file:
-    #     out_csv = csv.writer(file, lineterminator='\n')
-    #
-    #     columns = [column.name for column in inspect(User).columns][1:]
-    #     out_csv.writerow(columns)
-    #
-    #     session_3 = orm.sessionmaker()
-    #
-    #     extract_query = [getattr(User, col) for col in columns]
-    #     for mov in session_3.query(*extract_query):
-    #         out_csv.writerow(mov)
-    #
-    #     session_3.close()
-
-
-    # with open('mydump.csv', 'w') as f:
-    #     out = csv.writer(f)
-    #
-    #     out.writerow(['id', 'email', 'password', 'name'])
-    #     for item in session.query(Queue).all():
-    #         out.writerow([User.id, User.email, User.password, User.name])
-    #
-    #
 
     db.create_all(app=create_app())  # create the SQLite database
     app.run(debug=True)  # run the flask app on debug mode
-    # con = sqlite3.connect('data.db')
-    # outfile = open('mydump.csv', 'wb')
-    # outcsv = csv.writer(outfile)
-    #
-    # cursor = con.execute('select * from mytable')
-    #
-    # # dump column titles (optional)
-    # outcsv.writerow(x[0] for x in cursor.description)
-    # # dump rows
-    # outcsv.writerows(cursor.fetchall())
-    #
-    # outfile.close()
+

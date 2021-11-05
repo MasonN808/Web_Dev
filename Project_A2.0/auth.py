2da266a26bef00f57921b590c6e3e6ee1cbb98df
Mason Nakamura <Mason.Nakamura1@marist.edu>
2021-11-04 19:20:58 -1000

    - Removed 'username' attribute to user in mySQL in both signup() and login() - Progress in changing login() - Note: untested

--- Project_A2.0/auth.py
+++ Project_A2.0/auth.py
@@ -23,6 +23,27 @@ def login(): # define login page fucntion
         email = request.form.get('email')
         password = request.form.get('password')
         remember = True if request.form.get('remember') else False
+
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM accounts WHERE username = % s', (email,))
+    account = cursor.fetchone()
+    if account:
+        msg = 'Account already exists !'
+    elif not password or not email:
+        msg = 'Please fill out the form !'
+    else:
+        cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s)', (password, email,))
+        mysql.connection.commit()
+        msg = 'You have successfully registered !'
+
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+
+
+
+
+
+
         user = User.query.filter_by(email=email).first()
         # check if the user actually exists
         # take the user-supplied password, hash it, and compare it
@@ -77,23 +98,20 @@ def login(): # define login page fucntion
 @auth.route('/signup', methods =['GET', 'POST'])
 def signup():
     msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
-        username = request.form['username']
+    if request.method == 'POST' and 'password' in request.form and 'email' in request.form :
         password = request.form['password']
         email = request.form['email']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
+        cursor.execute('SELECT * FROM accounts WHERE email = % s', (email, ))
         account = cursor.fetchone()
         if account:
             msg = 'Account already exists !'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             msg = 'Invalid email address !'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers !'
-        elif not username or not password or not email:
+        elif not password or not email:
             msg = 'Please fill out the form !'
         else:
-            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s)', (password, email, ))
             mysql.connection.commit()
             msg = 'You have successfully registered !'
     elif request.method == 'POST':
